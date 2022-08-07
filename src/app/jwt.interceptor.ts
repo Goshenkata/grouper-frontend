0705b0d43609591fd1cf7d27e236b000f6ff0c05
Goshenkata <Koleto24@protonmail.com>
2022-08-07 14:29:24 +0300

    fixed refresh token bug

--- src/app/jwt.interceptor.ts
+++ src/app/jwt.interceptor.ts
@@ -55,9 +55,7 @@ export class JwtInterceptor implements HttpInterceptor {
             next: res => {
               localStorage.setItem('refresh_token', res.refresh_token)
               localStorage.setItem('access_token', res.access_token)
-              localStorage.setItem('role', res.role.toString())
               localStorage.setItem('expires_at', res.expires_at.toString())
-              localStorage.setItem('username', res.username)
               console.log('token refreshed')
               return next.handle(this.addTokenHeader(request, res.access_token));
             },
