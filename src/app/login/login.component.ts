26d2b9af11afc6753ded664917f0d4e50a37ca9c
Goshenkata <Koleto24@protonmail.com>
2022-08-13 13:55:37 +0300

    last monitue bug fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -37,8 +37,8 @@ export class LoginComponent implements OnInit {
         localStorage.setItem('expires_at', res.expires_at.toString())
         localStorage.setItem('role', res.role.toString())
       },
-      error: (err: HttpErrorResponse) => {
-        this.toastr.error()
+      error: (err) => {
+        this.toastr.error(err.status = 401 ? 'Wrong email or password' : "Something went wrong, we're working on it");
       },
       complete: () => this.router.navigateByUrl('/')
     });
